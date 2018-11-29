300e57aa31f4e8eeac0c41abce4580f2f6be14a6
Панин М.А <mic@alice.ru>
2018-11-29 11:44:30 +0300

    play with localStorage variable

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
-	function something() {
-		alert('Hello everyone!');
+	function something() 
+	{
+		var x = window.localStorage.getItem('aaa');
+		window.localStorage.setItem('aaa', 555);
+		alert(x);
 	}
