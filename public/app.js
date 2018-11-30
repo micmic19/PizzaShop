d6d7cf87c6a040c2bea17e3172ad885f289c6466
Панин М.А <mic@alice.ru>
2018-11-30 15:51:29 +0300

    Add add_to_cart function stub

--- public/app.js
+++ public/app.js
@@ -1,14 +1,12 @@
-function allStorage() 
-{
-    var archive = [];
-    for (var i = 0; i<localStorage.length; i++) {
-        archive[i] = localStorage.getItem(localStorage.key(i));
-}
-
 function something() 
 {
 	var x = window.localStorage.getItem('bbb');
 	x = x * 1 + 1;
 	window.localStorage.setItem('bbb', x);
 	alert(x);
+}
+
+function add_to_cart()
+{
+	alert('Hello from function');
 }
