208ffbfa4cfd0f4c5c041373bbdde1f28324fd02
Панин М.А <mic@alice.ru>
2018-11-30 15:42:14 +0300

    Display products, price and size

--- public/app.js
+++ public/app.js
@@ -1,15 +1,14 @@
 function allStorage() 
 {
-        var archive = [];
-        for (var i = 0; i<localStorage.length; i++) {
-            archive[i] = localStorage.getItem(localStorage.key(i));
+    var archive = [];
+    for (var i = 0; i<localStorage.length; i++) {
+        archive[i] = localStorage.getItem(localStorage.key(i));
 }
 
-	function something() 
-	{
-
-		var x = window.localStorage.getItem('bbb');
-		x = x * 1 + 1;
-		window.localStorage.setItem('bbb', x);
-		alert(x);
-	}
+function something() 
+{
+	var x = window.localStorage.getItem('bbb');
+	x = x * 1 + 1;
+	window.localStorage.setItem('bbb', x);
+	alert(x);
+}
