d2db40967a696e5f3f9dc3af289a2108731a6906
Панин М.А <mic@alice.ru>
2018-11-30 17:09:33 +0300

    javascript function total_items()

--- public/app.js
+++ public/app.js
@@ -9,8 +9,20 @@ function something()
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
-	
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+}
+
+function total_items()
+{
+	var total = 0;
+	for (var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+		if (key.match(/product_.*/)) {
+			total = total * 1. + parseInt(localStorage.getItem(key), 10);
+		}
+	}
+	return total;
 }
