3ec3d429f11d47e930370a11c7621b1fa0014de2
Панин М.А <mic@alice.ru>
2018-12-04 11:38:16 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -9,6 +9,21 @@ set :database, "sqlite3:pizzashop.db"
 class Product < ActiveRecord::Base
 end
 
+def parse_order_line orders_line
+	
+	s1 = orders_line.split(/,/)
+	arr = []
+	s1.each do |x|
+		s2 = x.split(/\=/)
+		s3 = s2[0].split(/_/)
+		id = s3[1]
+		cnt = s2[1]
+		
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+	return arr
+end
 
 get '/' do
 	@products = Product.all
@@ -20,6 +35,6 @@ get '/about' do
 end
 
 post '/cart' do
-	
-	erb 'Hello'
+	@orders = parse_order_line(params[:orders])
+	erb "Hello! #{@orders.inspect}"
 end
